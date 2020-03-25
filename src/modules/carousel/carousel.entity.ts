924b81962f62ce3ef9d3026bfba1925e0317a6f9
Chukwuma Ezumezu <chukwuma.ezumezu@gmail.com>
2020-03-25 19:41:38 +0100

    uuid for pk

--- src/modules/carousel/carousel.entity.ts
+++ src/modules/carousel/carousel.entity.ts
@@ -1,16 +1,22 @@
 import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from 'typeorm';
+import { ObjectType, Field } from '@nestjs/graphql';
 
+@ObjectType()
 @Entity()
 export class Carousel extends BaseEntity {
-  @PrimaryGeneratedColumn()
-  id: number;
+  @Field()
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
 
+  @Field()
   @Column()
   title: string;
 
+  @Field()
   @Column()
   description: string;
 
+  @Field()
   @Column()
   imageUrl: string;
 }
